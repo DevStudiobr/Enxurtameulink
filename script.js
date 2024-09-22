82b6eda5bb26d90ec91e5f17e221bf6808c4bbc3
thiagooliveira16 <118765402+thiagooliveira16@users.noreply.github.com>
2024-09-22 17:48:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById('shortenBtn').addEventListener('click', async () => {
     }
 
     try {
-        const response = await fetch('http://127.0.0.1:5000/shorten', {
+const response = await fetch('http://<SEU_IP_LOCAL>:5000/shorten', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
